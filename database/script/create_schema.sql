c232929dca2ebbbd7fc4269d4f9ce9c22939ca86
ee60235 <ee60235>
2021-04-09 16:54:37 +0200

    settings

--- database/script/create_schema.sql
+++ database/script/create_schema.sql
@@ -1,4 +1,5 @@
-create database workstocks;
-
-CREATE USER 'workstocks_user'@'localhost' IDENTIFIED BY 'workstocks_password';
+CREATE USER IF NOT EXISTS 'workstocks_user'@'localhost' IDENTIFIED BY 'workstocks_password';
 GRANT ALL PRIVILEGES ON * . * TO 'workstocks_user'@'localhost';
+
+DROP DATABASE IF EXISTS workstocks;
+CREATE DATABASE workstocks;
