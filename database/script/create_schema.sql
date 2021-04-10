b875e97f2935fea1e20ee475b99e1f9e8d11d2de
enrimon15 <38566410+enrimon15@users.noreply.github.com>
2021-04-10 12:07:14 +0200

    update create schema

--- database/script/create_schema.sql
+++ database/script/create_schema.sql
@@ -1,5 +1,5 @@
-CREATE USER IF NOT EXISTS 'workstocks_user'@'localhost' IDENTIFIED BY 'workstocks_password';
-GRANT ALL PRIVILEGES ON * . * TO 'workstocks_user'@'localhost';
-
 DROP DATABASE IF EXISTS workstocks;
 CREATE DATABASE workstocks;
+
+CREATE USER IF NOT EXISTS 'workstocks_user'@'localhost' IDENTIFIED BY 'workstocks_password';
+GRANT ALL PRIVILEGES ON workstocks.* TO 'workstocks_user'@'localhost';
