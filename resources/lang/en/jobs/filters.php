bb96ec67947d9686673234887582230a8a7a64b0
Marco Ovidi <marco.ovidi@student.univaq.it>
2021-04-18 20:04:16 +0200

    dynamic tags for skills most popular

---
+++ resources/lang/en/jobs/filters.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'skillsTitle' => 'Skills',
+    'experienceTitle' => 'Experience',
+    'jobTypeTitle' => 'Job Type',
+    'jobLevelTitle' => 'Ruolo',
+    'expectedSalleryTitle' => 'Expected Sallery',
+    'year'=> 'Year',
+    'years' => 'Years',
+    'fullTime' => 'Full time',
+    'partTime' => 'Part Time',
+    'constructionBase' => 'Construction Base',
+    'Internship' => 'Internship',
+];
---
+++ resources/lang/it/jobs/filters.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'skillsTitle' => 'Abilità',
+    'experienceTitle' => 'Esperienza Richiesta',
+    'jobTypeTitle' => 'Tipo di Contratto',
+    'jobLevelTitle' => 'Ruolo',
+    'expectedSalleryTitle' => 'Retribuzione',
+    'year'=> 'Anno',
+    'years' => 'Anni',
+    'fullTime' => 'Tempo Pieno',
+    'partTime' => 'Part Time',
+    'constructionBase' => 'Apprendistato',
+    'Internship' => 'Tirocinio',
+];
