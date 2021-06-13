7b7bba81a4300d36757f52550f31212a8c048d07
enrimon15 <38566410+enrimon15@users.noreply.github.com>
2021-06-13 17:15:10 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ composer install
 php artisan migrate:fresh
 php artisan db:seed --class=DatabaseSeeder
 php artisan db:seed --class=MenuSeeder
+php artisan db:seed --class=MenuItemSeeder
 php artisan db:seed --class=HomeSeeder
 php artisan voyager:install
 php artisan voyager:admin admin@admin.it
